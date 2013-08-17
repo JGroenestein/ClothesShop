1a5d91966a326bb0760a0b1210daecaf24c445ed
Joshua Groenestein <j.groenestein@gmail.com>
2013-08-17 17:31:19 +1000

    2nd initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClothesShopBeta::Application.config.secret_token = '0dcd3d1e9c68b163fa8b60132a95cd5fa48e4d93fa04ae951e25698f802cd184286e559a7e8c6c72248604f8ec2bf8fadd8a3a4682a1e520a43aa4b86781b13a'
